c2698dce2d7d4bd3a7690ced536ec44f46c1dbc8
Timmy Omolana <timmieomolanakiel@gmail.com>
2023-04-18 18:10:07 +0100

    Update: improve error handling

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -3,6 +3,12 @@ import { handleResponse } from '../handlers/responseHandler.js';
 
 export const handleError = (err, _req, res, _next) => {
   if (err instanceof AppError) {
+    console.error(err);
+
+    if (err.message.includes('E11000')) {
+      return handleResponse(res, 400, { error: "User already exists..."})
+    }
+
     return handleResponse(res, err.statusCode, { error: err.message });
   }
 
