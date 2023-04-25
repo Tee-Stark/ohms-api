040ba499f312f819d41e5390180bd7eb70780921
Timmy Omolana <timmieomolanakiel@gmail.com>
2023-04-25 15:15:09 +0100

    Update: add validation to auth routes

--- router/auth.router.js
+++ router/auth.router.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { Login, SignUp } from '../controllers/auth.controller.js';
+import { ValidateUser } from '../validations/user.validation.js';
 
 const authRouter = express.Router();
 
-authRouter.post('/signup', SignUp)
-authRouter.post('/login', Login);
+authRouter.post('/signup', ValidateUser, SignUp)
+authRouter.post('/login', ValidateUser, Login);
 
 export default authRouter;
--- validations/user.validation.js
+++ validations/user.validation.js
@@ -1,9 +1,8 @@
 import asyncHandler from "../handlers/asyncHandler.js";
 import userSchema from "./schema/user.schema.js";
 
-const ValidateUser = asyncHandler(async (req, res, next) => {
+export const ValidateUser = asyncHandler(async (req, res, next) => {
     const valid = await userSchema.validateAsync(req.body);
     return next();
 })
 
-export default ValidateUser;
