58127bd62c2a174e4a9add97c1863a23f5c0e9dd
Timmy Omolana <timmieomolanakiel@gmail.com>
2023-04-25 20:58:42 +0100

    Update: add router to get all

--- router/application.router.js
+++ router/application.router.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import { GetApplication, SearchUserApplication, SubmitApplication, UpdateApplicationStatus } from "../controllers/application.controller.js";
+import { GetAllApplications, GetApplication, SearchUserApplication, SubmitApplication, UpdateApplicationStatus } from "../controllers/application.controller.js";
 import { requireAdmin, requireAuth } from '../middlewares/auth.middleware.js';
 
 const applicationRouter = express.Router();
 
 applicationRouter.post('/create', requireAuth, SubmitApplication);
-applicationRouter.get('/:id', requireAuth, GetApplication);
-applicationRouter.get('/review', [requireAuth, requireAdmin], UpdateApplicationStatus);
 applicationRouter.get('/search', requireAuth, SearchUserApplication);
+applicationRouter.get('/', [requireAuth, requireAdmin], GetAllApplications);
+applicationRouter.get('/:id', requireAuth, GetApplication);
+applicationRouter.get('/:id/review', [requireAuth, requireAdmin], UpdateApplicationStatus);
+
 
 export default applicationRouter;
