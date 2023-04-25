27f8ad975feea194b20eb29b7f0e62f44b41b602
Timmy Omolana <timmieomolanakiel@gmail.com>
2023-04-25 20:56:29 +0100

    update: adjust error status handling

--- error/AppError.js
+++ error/AppError.js
@@ -1,7 +1,7 @@
 export class AppError extends Error {
     constructor(message, statusCode=400) {
         super(message);
-        this.statusCode = statusCode;
+        this.statusCode = statusCode || 500;
         this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
         this.isOperational = true;
         Error.captureStackTrace(this, this.constructor);
