a8a552991ebddc6347943467b706e296a5891096
Timmy Omolana <timmieomolanakiel@gmail.com>
2023-04-25 20:54:31 +0100

    update: add role to schema

--- validations/schema/user.schema.js
+++ validations/schema/user.schema.js
@@ -3,6 +3,7 @@ import Joi from '@hapi/joi'
 const userSchema = Joi.object().keys({
     email: Joi.string().email().required(),
     password: Joi.string().min(6).max(32).required(),
+    role: Joi.string().allow('user', 'admin').optional()
  });
   
   export default userSchema;
