6ed8db45a1ed6907f4d07d0a563a18a51ec1bc4b
Timmy Omolana <timmieomolanakiel@gmail.com>
2023-04-25 20:56:54 +0100

    update: change default to 500

--- handlers/responseHandler.js
+++ handlers/responseHandler.js
@@ -5,11 +5,11 @@ const Responses = {
     400: "You might be missing required parameters",
     401: "You, my friend are not authenticated. Thanks, and do the needful!",
     403: "You, my friend are not authorized to carry out this operation. Thanks, and do the needful!",
-    default: "Something went wrong internally... Just chill, we've got this!"
+    500: "Something went wrong internally... Just chill, we've got this!"
 }
 
 
 export function handleResponse(res, status, data) {
-    const message = Responses[status] || Responses.default;
+    const message = Responses[status];
     res.status(status).json({ status, message, data });
 }
