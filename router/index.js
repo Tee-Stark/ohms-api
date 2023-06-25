da4a25114515f45e31189cf3a02d1769b610d9d5
Timmy Omolana <timmieomolanakiel@gmail.com>
2023-06-25 13:37:21 +0100

    Update: add new routers

--- router/index.js
+++ router/index.js
@@ -3,6 +3,9 @@ import authRouter from './auth.router.js';
 import indexRouter from './index.router.js';
 import applicationRouter from './application.router.js';
 import complaintRouter from './complaint.router.js';
+import roomRouter from './room.router.js';
+import userRouter from './user.router.js';
+import uploadRouter from './uploads.router.js';
 
 const appRouter = express.Router();
 
@@ -13,6 +16,9 @@ appRouter.get('/', (_req, res) => {
 appRouter.use('/', indexRouter);
 appRouter.use('/auth', authRouter);
 appRouter.use('/application', applicationRouter);
-appRouter.use('/complaint', complaintRouter)
+appRouter.use('/complaint', complaintRouter);
+appRouter.use('/room', roomRouter);
+appRouter.use('/user', userRouter);
+appRouter.use('/upload', uploadRouter);
 
 export default appRouter;
