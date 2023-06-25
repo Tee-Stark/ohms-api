77a9ed12eff356e50172980b16bbee765ae42ffe
Timmy Omolana <timmieomolanakiel@gmail.com>
2023-06-25 13:39:23 +0100

    Update: implement filter

--- services/complaint.service.js
+++ services/complaint.service.js
@@ -28,8 +28,26 @@ const GetComplaints = async () => {
     }
 }
 
+const GetComplaintsByDate = async (date) => {
+    try {
+        const startOfDay = new Date(date);
+        startOfDay.setHours(0, 0, 0, 0);
+        const endOfDay = new Date(date);
+        endOfDay.setHours(23, 59, 59, 999);
+
+        const complaints = await Complaint.find({
+            createdAt: { $gte: startOfDay, $lte: endOfDay }
+        }).lean();
+
+        return complaints;
+    } catch (error) {
+        throw new AppError(error, 500);
+    }
+};
+
 export default {
     CreateComplaint,
     GetComplaint,
-    GetComplaints
+    GetComplaints,
+    GetComplaintsByDate
 }
