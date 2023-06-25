aff09f2efbc15c6f8e5357397d3a5e005789dd7e
Timmy Omolana <timmieomolanakiel@gmail.com>
2023-06-25 13:27:12 +0100

    Update: change import style for connectDB (into destructured)

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import app from './app.js';
-import db from './config/db.config.js'
+import { connectDB } from './config/db.config.js'
 import { PORT } from './config/constants.config.js';
 
 const server = app.listen(PORT, async () => {
-    await db.connectDB();
+    await connectDB();
     console.log(`App listening on port: ${PORT}`);
 });
