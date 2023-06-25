19cbaeedb0fbc0b5e202e95515a5415abbc93321
Timmy Omolana <timmieomolanakiel@gmail.com>
2023-06-25 13:36:55 +0100

    Update: add filter endpoint

--- router/complaint.router.js
+++ router/complaint.router.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { requireAdmin, requireAuth } from '../middlewares/auth.middleware.js';
-import { GetAllComplaints, SubmitComplaint } from '../controllers/complaint.controller.js';
+import { GetAllComplaints, GetComplaintsByDate, SubmitComplaint } from '../controllers/complaint.controller.js';
 import { ValidateComplaint } from '../validations/complaint.validation.js';
 
 const complaintRouter =  express.Router();
 
 complaintRouter.post('/', [ValidateComplaint, requireAuth], SubmitComplaint)
 complaintRouter.get('/', [requireAuth, requireAdmin], GetAllComplaints)
+complaintRouter.get('/filter', [requireAuth, requireAdmin], GetComplaintsByDate)
 
 export default complaintRouter;
